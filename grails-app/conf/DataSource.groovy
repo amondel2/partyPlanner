7437c93f69f6f6326c3a5171c56a77ae44aa60f5
amondel2 <amondel2@gmail.com>
2013-05-06 09:29:48 -0300

    Update DataSource.groovy
    remove passwords

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -2,8 +2,7 @@ dataSource {
     pooled = true
     driverClassName = "com.mysql.jdbc.Driver"
 //    driverClassName = "org.postgresql.Driver"
-    username = "A878499_wed"
-    password = "Splatt66"
+
 //	dialect = org.hibernate.dialect.PostgreSQLDialect
 }
 hibernate {
@@ -16,8 +15,8 @@ environments {
     development {
         dataSource {
             dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:mysql://mysql1122.ixwebhosting.com/A878499_wed_plan?autoreconnect=true"
-//            url = "jdbc:postgresql://pgsql1101.ixwebhosting.com/A878499_wed_plan"
+
+
 			properties {
 				maxActive = -1
 				minEvictableIdleTimeMillis=1800000
@@ -34,15 +33,14 @@ environments {
     test {
         dataSource {
             dbCreate = "create"
-            url = "jdbc:mysql://mysql1122.ixwebhosting.com/A878499_wed_plan?autoreconnect=true"
-            		//url = "jdbc:postgresql://pgsql1101.ixwebhosting.com/A878499_wed_plan"
+
+            		
         }
     }
     production {
         dataSource {
             dbCreate = "create"
-            url = "jdbc:mysql://mysql1122.ixwebhosting.com/A878499_wed_plan?autoreconnect=true"
-            		//url = "jdbc:postgresql://pgsql1101.ixwebhosting.com/A878499_wed_plan"
+
             pooled = true
             properties {
                maxActive = -1
