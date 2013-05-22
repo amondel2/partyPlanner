9f82d70be862bf155bfa05eb2b951ffe300d9e68
Aaron <aaron@mondelblatt.com>
2013-05-22 06:20:37 -0400

    Fixed initPassword not being used when creating the user

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -2,8 +2,8 @@ import com.muhlsoftware.wedding.Role
 import com.muhlsoftware.wedding.User
 import com.muhlsoftware.wedding.UserRole
 
-class BootStrap {
 
+class BootStrap {	
     def init = { servletContext ->
 		//Make sure the following Roles exists if not create them
 		def adminRole = Role.findByAuthority('ROLE_ADMIN')
@@ -14,10 +14,10 @@ class BootStrap {
 			new Role(authority: 'ROLE_USER').save(flush: true)
 		}
 		def superUserName =  'you'  //Please change this to something else
-		def initPassword =  '@yourFirstPword' //Please change this to something else
+		def initPassword =  'changeme123' //Please change this to something else
 		def testUser = User.findByUsername(superUserName)
 		if(!testUser) {
-			testUser = new User(username: superUserName, enabled: true, password: 'password')
+			testUser = new User(username: superUserName, enabled: true, password: initPassword)
 			testUser.save(flush: true)
 		}
 		if(!UserRole.findByUser(testUser)) {		
